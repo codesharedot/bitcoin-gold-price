d9b39ff1c15a7048ffb1e760eb5e911becf4829b
codesharedot <codeto@sent.com>
2019-09-12 11:07:57 +0200

    first commit

--- example.py
+++ example.py
@@ -7,14 +7,14 @@ c = CurrencyRates()
 rate = c.get_rate('USD', 'EUR') 
 print(rate)
 
-litecoin_api_url = 'https://api.coinmarketcap.com/v1/ticker/litecoin/'
-response = requests.get(litecoin_api_url)
+eos_api_url = 'https://api.coinmarketcap.com/v1/ticker/eos/'
+response = requests.get(eos_api_url)
 response_json = response.json()
 print(response_json)
 
 for coin in response.json():
     price = coin.get("price_usd", "U$S Price not provided")
-    litecoin_price = float(("{0:.2f}").format(float(price)))
-    print("$ " + str(litecoin_price))
-    litecoin_price_eur = float(("{0:.2f}").format(float(price)*rate))   
-    print("€ " + str(litecoin_price_eur))
+    eos_price = float(("{0:.2f}").format(float(price)))
+    print("$ " + str(eos_price))
+    eos_price_eur = float(("{0:.2f}").format(float(price)*rate))   
+    print("€ " + str(eos_price_eur))
